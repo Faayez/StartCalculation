e3957e5cf7cbda497bc165fc3907eb76c02d180f
Ibrahim F <faayez@gmail.com>
2021-08-28 21:27:36 +0200

    commit 4

--- StartCalculation.Web/Controllers/CalculationController.cs
+++ StartCalculation.Web/Controllers/CalculationController.cs
@@ -1,16 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using StartCalculation.Services.ViewModels;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace StartCalculation.Web.Controllers
 {
     public class CalculationController : Controller
     {
+        private const string ApiUrl = "https://localhost:44350/Calculations";
         private readonly ILogger<CalculationController> _logger;
 
         public CalculationController(ILogger<CalculationController> logger)
         {
             _logger = logger;
+        }
+
+        public async Task<IActionResult> Detail(Guid id)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync($"{ApiUrl}?id={id}");
+            var result = await response.Content.ReadAsStringAsync();
+            var data = JsonSerializer.Deserialize<CalculationStatusDto>(result);
+            return View(data);
         }
 
         public IActionResult Create()
@@ -20,9 +35,18 @@ namespace StartCalculation.Web.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(CalculationInsertDto calculation)
+        public async Task<IActionResult> Create(CalculationInsertDto calculation)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                var client = new HttpClient();
+                var response = await client.PostAsync(ApiUrl, new StringContent(JsonSerializer.Serialize(calculation), Encoding.UTF8, "application/json"));
+                return RedirectToAction(nameof(Create));
+            }
+            else
+            {
+                return View(calculation);
+            }
         }
     }
 }
